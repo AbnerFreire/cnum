fe4486ec40b5486a40200f2f1271da6cd8981da5
abnersf <54453516+AbnerFreire@users.noreply.github.com>
2022-10-10 18:20:08 +0000

    feito

--- 5/main.jl
+++ 5/main.jl
@@ -1,53 +1,36 @@
-using LinearAlge
+using LinearAlgebra
 
-A = [1 1 1 ;
-     4 4 2 ;
+A = [1 1 1;
+     4 4 2;
      2 1 -1]
 
-B = [1 ; 2 ; 0]
+B = [1; 2; 0]
 
-X = A \ B
-println(X)
+x = A \ B
+println(x)
 
-error = 10^-3
+erro = 10^-3
 
-function jacobi (A, B, K)
- n = size(B,1)
- X = zeros(n)
- K = zeros(n)
- for l = 1: K
-    for i + 1:n
+function jacobi(A, B, K)
+  n = size(b, 1)
+  X = zeros(n)
+  K = zeros(n)
+  for l = 1:k
+    for i = 1:n
       count = 0
       for j = 1:n
-        if i !=j
-          count += A[i,j]* X[j]
-        end
+        if i != j
+        count += A[i, j]*X[j]
       end
-    K[i] = (B[i])-count)/A[i,i]
     end
-    println(norm(X-K))
-    if norm (X-K) < error
-      break
-    end
-    X = copy(K)
+    k[i] = (B[i]-count)/A[i, i]
   end
-  return X
+
+  if norm(X.K) < erro
+    break
+  end
+  x = copy(K)
 end
-
-X = jacobi[A, B, 10]
-println(X)
-
-A = [2 1 -1 ;
-     4 4 2 ;
-     1 1 1]
-
-B = [0 ; 2 ;1]
-
-X = jacobi(A, B, 10)
-println(X)
-
-function seidel (A, B, K)
-  n = size(B,1)
-  x = zeros(n)
-  K = zeros(n)
+return x
+end
   
